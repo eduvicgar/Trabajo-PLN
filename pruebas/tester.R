720aea3075430104a337691701512e0c5c91b7f5
eduvicgar <eduvg0102@gmail.com>
2025-01-17 17:28:11 +0100

    comentarios tabla latex

--- pruebas/tester.R
+++ pruebas/tester.R
@@ -8,7 +8,7 @@ source("../fuentes/buscar_definicion_diccionario.R")
 source("../fuentes/filtrar_enfermedades.R")
 
 # Leemos el texto a analizar
-texto <- readLines("../datos/texto 19718.txt")
+texto <- readLines("../datos/texto 120.txt")
 
 # Obtenemos el dataframe
 resultado <- filtrar_enfermedades(texto)
